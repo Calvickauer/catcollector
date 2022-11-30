1d196ebe4dc8429ce9fe14698afcd099c8edcc17
calvickauer <calvickauer@gmail.com>
2022-11-29 21:39:43 -0800

    Django app

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name="contact"),
-    path('blog/', views.contact, name="blog"),
+    path('blog/', views.blog, name="blog"),
 ]
